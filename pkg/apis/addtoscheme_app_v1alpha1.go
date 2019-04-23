1f0d4e39de65431f90131eb212a4f57f8f8f5dd0
tchughesiv <tchughesiv@gmail.com>
2019-04-23 18:39:34 -0500

    fixes
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- pkg/apis/addtoscheme_app_v1alpha1.go
+++ pkg/apis/addtoscheme_app_v1alpha1.go
@@ -2,9 +2,19 @@ package apis
 
 import (
 	"github.com/kiegroup/submarine-cloud-operator/pkg/apis/app/v1alpha1"
+	oappsv1 "github.com/openshift/api/apps/v1"
+	buildv1 "github.com/openshift/api/build/v1"
+	oimagev1 "github.com/openshift/api/image/v1"
+	routev1 "github.com/openshift/api/route/v1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes,
+		v1alpha1.SchemeBuilder.AddToScheme,
+		oappsv1.SchemeBuilder.AddToScheme,
+		routev1.SchemeBuilder.AddToScheme,
+		oimagev1.SchemeBuilder.AddToScheme,
+		buildv1.SchemeBuilder.AddToScheme,
+	)
 }
