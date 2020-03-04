837e13b57d587e527e3193505c6db18edc925d8a
Ricardo Zanini <zanini@redhat.com>
2020-03-04 14:36:39 -0300

    [KOGITO-993] Removing annoying symlinks from protobuf configMaps
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- pkg/infrastructure/services/kafka.go
+++ pkg/infrastructure/services/kafka.go
@@ -20,20 +20,11 @@ import (
 	kafkabetav1 "github.com/kiegroup/kogito-cloud-operator/pkg/apis/kafka/v1beta1"
 	"github.com/kiegroup/kogito-cloud-operator/pkg/client"
 	"github.com/kiegroup/kogito-cloud-operator/pkg/infrastructure"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"strings"
 )
 
 const (
-	kafkaClusterLabel string = "strimzi.io/cluster"
-
 	kafkaURINotFoundError string = "there's no Kafka instance URI found in the namespace and Kafka external URI is not specified, cannot deploy Data Index service"
-	kafkaNotExist         string = "there is no Kafka instance found in the namespace, cannot deploy Kafka Topics"
-
-	kafkaTopicConfigRetentionKey   string = "retention.ms"
-	kafkaTopicConfigSegmentKey     string = "segment.bytes"
-	kafkaTopicConfigRetentionValue string = "604800000"
-	kafkaTopicConfigSegmentValue   string = "1073741824"
 
 	quarkusTopicBootstrapEnvVar = "MP_MESSAGING_%s_%s_BOOTSTRAP_SERVERS"
 	quarkusBootstrapEnvVar      = "QUARKUS_KAFKA_BOOTSTRAP_SERVERS"
@@ -62,45 +53,9 @@ func getKafkaServerURI(kafkaProp v1alpha1.KafkaConnectionProperties, namespace s
 	return "", fmt.Errorf(kafkaURINotFoundError)
 }
 
-func getKafkaServerReplicas(kafkaProp v1alpha1.KafkaConnectionProperties, namespace string, client *client.Client) (string, int32, error) {
-	if len(kafkaProp.ExternalURI) <= 0 {
-		if kafka, err := getKafkaInstance(kafkaProp, namespace, client); err != nil {
-			return "", 0, err
-		} else if kafka != nil {
-			if replicas := infrastructure.ResolveKafkaServerReplicas(kafka); replicas > 0 {
-				return kafka.Name, replicas, nil
-			}
-		}
-
-		return "", 0, fmt.Errorf(kafkaNotExist)
-	}
-
-	return "", 0, nil
-}
-
 func getKafkaInstance(kafka v1alpha1.KafkaConnectionProperties, namespace string, client *client.Client) (*kafkabetav1.Kafka, error) {
 	if len(kafka.Instance) > 0 {
 		return infrastructure.GetKafkaInstanceWithName(kafka.Instance, namespace, client)
 	}
 	return nil, nil
-}
-
-func newKafkaTopic(topicName string, kafkaName string, kafkaReplicas int32, namespace string) *kafkabetav1.KafkaTopic {
-	return &kafkabetav1.KafkaTopic{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      topicName,
-			Namespace: namespace,
-			Labels: map[string]string{
-				kafkaClusterLabel: kafkaName,
-			},
-		},
-		Spec: kafkabetav1.KafkaTopicSpec{
-			Replicas:   kafkaReplicas,
-			Partitions: 10,
-			Config: map[string]string{
-				kafkaTopicConfigRetentionKey: kafkaTopicConfigRetentionValue,
-				kafkaTopicConfigSegmentKey:   kafkaTopicConfigSegmentValue,
-			},
-		},
-	}
 }
