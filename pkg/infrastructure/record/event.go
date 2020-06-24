a436379b63f7667510ca1e8d904d3aeb18875f20
Vaibhav jain <vaibhavjain.wiz@gmail.com>
2020-06-24 11:30:25 +0530

    [KOGITO-1584] - Code formatting

--- pkg/infrastructure/record/event.go
+++ pkg/infrastructure/record/event.go
@@ -37,8 +37,7 @@ type EventRecorder interface {
 	Eventf(cli *client.Client, object runtime.Object, eventType, reason, messageFmt string, args ...interface{})
 }
 
-// NewRecorder returns an EventRecorder that can be used to send events with the event source
-// set to the given event source.
+// NewRecorder returns an EventRecorder that can be used to send events with the given event source.
 func NewRecorder(scheme *runtime.Scheme, source corev1.EventSource) EventRecorder {
 	return &recorderImpl{scheme, source}
 }
@@ -97,11 +96,15 @@ func makeEvent(ref *corev1.ObjectReference, eventType, reason, message string) *
 	}
 }
 
+// GetHostName returns OS's hostname
 func GetHostName() string {
 	hostName, err := os.Hostname()
 	if err != nil {
 		return ""
 	}
+
+	// Trim whitespaces first to avoid getting an empty hostname
+	// For linux, the hostname is read from file /proc/sys/kernel/hostname directly
 	hostName = strings.TrimSpace(hostName)
 	if len(hostName) == 0 {
 		return ""
--- pkg/infrastructure/record/event_test.go
+++ pkg/infrastructure/record/event_test.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Red Hat, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package record
 
 import (
@@ -23,7 +37,9 @@ func Test_generateEvent(t *testing.T) {
 	recorder.Eventf(cli, service, "Normal", "Created", "Create Deployment")
 
 	eventList := &corev1.EventList{}
-	kubernetes.ResourceC(cli).ListWithNamespace(t.Name(), eventList)
+	if err := kubernetes.ResourceC(cli).ListWithNamespace(t.Name(), eventList); err != nil {
+		assert.Fail(t, "Not able to load kubernetes resources", err)
+	}
 	assert.NotNil(t, eventList.Items)
 	assert.Equal(t, 1, len(eventList.Items))
 }
@@ -40,6 +56,8 @@ func Test_generateEvent_InvalidEventType(t *testing.T) {
 	recorder.Eventf(cli, service, "InvalidEventType", "Created", "Create Deployment")
 
 	eventList := &corev1.EventList{}
-	kubernetes.ResourceC(cli).ListWithNamespace(t.Name(), eventList)
+	if err := kubernetes.ResourceC(cli).ListWithNamespace(t.Name(), eventList); err != nil {
+		assert.Fail(t, "Not able to load kubernetes resources", err)
+	}
 	assert.Nil(t, eventList.Items)
 }
