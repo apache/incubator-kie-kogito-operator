58d645d02b5b413e8364d50582efa4624a677d67
jake <jacopo.r00ta@gmail.com>
2020-07-03 16:32:29 +0200

    add trusty resources

---
+++ pkg/apis/app/v1alpha1/kogitotrusty_types.go
@@ -0,0 +1,101 @@
+// Copyright 2019 Red Hat, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// KogitoTrustyCRDName is the name of the Kogito Trusty CRD in the cluster.
+const KogitoTrustyCRDName = "kogitotrusty.app.kiegroup.org"
+
+// KogitoTrustySpec defines the desired state of KogitoTrusty.
+// +k8s:openapi-gen=true
+type KogitoTrustySpec struct {
+	InfinispanMeta    `json:",inline"`
+	KafkaMeta         `json:",inline"`
+	KogitoServiceSpec `json:",inline"`
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// GetRuntime ...
+func (d *KogitoTrustySpec) GetRuntime() RuntimeType {
+	return QuarkusRuntimeType
+}
+
+// KogitoTrustyStatus defines the observed state of KogitoTrusty.
+// +k8s:openapi-gen=true
+type KogitoTrustyStatus struct {
+	KogitoServiceStatus `json:",inline"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KogitoTrusty defines the Trusty Service infrastructure deployment.
+// +k8s:openapi-gen=true
+// +kubebuilder:resource:path=kogitodataindices,scope=Namespaced
+// +kubebuilder:printcolumn:name="Replicas",type="integer",JSONPath=".spec.replicas",description="Number of replicas set for this service"
+// +kubebuilder:printcolumn:name="Image",type="string",JSONPath=".status.image",description="Base image for this service"
+// +kubebuilder:printcolumn:name="Endpoint",type="string",JSONPath=".status.externalURI",description="External URI to access this service"
+// +operator-sdk:gen-csv:customresourcedefinitions.displayName="Kogito Trusty"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Deployments,apps/v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Routes,route.openshift.io/v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="ConfigMaps,v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Services,v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="KafkaTopics,kafka.strimzi.io/v1beta1"
+type KogitoTrusty struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KogitoTrustySpec   `json:"spec,omitempty"`
+	Status KogitoTrustyStatus `json:"status,omitempty"`
+}
+
+// GetSpec ...
+func (k *KogitoTrusty) GetSpec() KogitoServiceSpecInterface {
+	return &k.Spec
+}
+
+// GetStatus ...
+func (k *KogitoTrusty) GetStatus() KogitoServiceStatusInterface {
+	return &k.Status
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KogitoTrustyList contains a list of KogitoTrusty.
+type KogitoTrustyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	// +listType=atomic
+	Items []KogitoTrusty `json:"items"`
+}
+
+// GetItemsCount ...
+func (l *KogitoTrustyList) GetItemsCount() int {
+	return len(l.Items)
+}
+
+// GetItemAt ...
+func (l *KogitoTrustyList) GetItemAt(index int) KogitoService {
+	if len(l.Items) > index {
+		return KogitoService(&l.Items[index])
+	}
+	return nil
+}
+
+func init() {
+	SchemeBuilder.Register(&KogitoTrusty{}, &KogitoTrustyList{})
+}
---
+++ pkg/infrastructure/trusty.go
@@ -0,0 +1,105 @@
+// Copyright 2019 Red Hat, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package infrastructure
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1"
+	"github.com/kiegroup/kogito-cloud-operator/pkg/client"
+	"github.com/kiegroup/kogito-cloud-operator/pkg/client/kubernetes"
+	"github.com/kiegroup/kogito-cloud-operator/pkg/framework"
+)
+
+const (
+	// DefaultTrustyImageName is just the image name for the Data Index Service
+	DefaultTrustyImageName = "kogito-trusty"
+	// DefaultTrustyName is the default name for the Data Index instance service
+	DefaultTrustyName = "trusty"
+
+	trustyHTTPRouteEnv = "KOGITO_TRUSTY_HTTP_URL"
+	trustyWSRouteEnv   = "KOGITO_TRUSTY_WS_URL"
+)
+
+// InjectTrustyURLIntoKogitoApps will query for every KogitoApp in the given namespace to inject the Data Index route to each one
+// Won't trigger an update if the KogitoApp already has the route set to avoid unnecessary reconciliation triggers
+func InjectTrustyURLIntoKogitoApps(client *client.Client, namespace string) error {
+	log.Debugf("Querying KogitoApps in the namespace '%s' to inject Trusty Route ", namespace)
+
+	dcs, err := getKogitoAppsDCs(namespace, client)
+	if err != nil {
+		return err
+	}
+	log.Debugf("Found %s KogitoApps in the namespace '%s' ", len(dcs), namespace)
+	var dataIndexEndpoints ServiceEndpoints
+	if len(dcs) > 0 {
+		log.Debug("Querying Data Index route to inject into KogitoApps ")
+		dataIndexEndpoints, err = GetDataIndexEndpoints(client, namespace)
+		if err != nil {
+			return err
+		}
+		log.Debugf("Trusty route is '%s'", dataIndexEndpoints.HTTPRouteURI)
+	}
+
+	for _, dc := range dcs {
+		// here we compare the current value to avoid updating the app every time
+		if len(dc.Spec.Template.Spec.Containers) == 0 {
+			break
+		}
+		updateHTTP := framework.GetEnvVarFromContainer(dataIndexEndpoints.HTTPRouteEnv, &dc.Spec.Template.Spec.Containers[0]) != dataIndexEndpoints.HTTPRouteURI
+		updateWS := framework.GetEnvVarFromContainer(dataIndexEndpoints.WSRouteEnv, &dc.Spec.Template.Spec.Containers[0]) != dataIndexEndpoints.WSRouteURI
+		if updateHTTP {
+			log.Debugf("Updating dc '%s' to inject route %s ", dc.GetName(), dataIndexEndpoints.HTTPRouteURI)
+			framework.SetEnvVar(dataIndexEndpoints.HTTPRouteEnv, dataIndexEndpoints.HTTPRouteURI, &dc.Spec.Template.Spec.Containers[0])
+		}
+		if updateWS {
+			log.Debugf("Updating dc '%s' to inject route %s ", dc.GetName(), dataIndexEndpoints.WSRouteURI)
+			framework.SetEnvVar(dataIndexEndpoints.WSRouteEnv, dataIndexEndpoints.WSRouteURI, &dc.Spec.Template.Spec.Containers[0])
+		}
+		// update only once
+		if updateWS || updateHTTP {
+			if err := kubernetes.ResourceC(client).Update(&dc); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// GetTrustyEndpoints queries for the Data Index URIs
+func GetTrustyEndpoints(client *client.Client, namespace string) (trustyEndpoints ServiceEndpoints, err error) {
+	route := ""
+	trustyEndpoints = ServiceEndpoints{HTTPRouteEnv: dataIndexHTTPRouteEnv, WSRouteEnv: dataIndexWSRouteEnv}
+	route, err = getSingletonKogitoServiceRoute(client, namespace, &v1alpha1.KogitoDataIndexList{})
+	if err != nil {
+		return
+	}
+	if len(route) > 0 {
+		var routeURL *url.URL
+		routeURL, err = url.Parse(route)
+		if err != nil {
+			log.Warnf("Failed to parse data index route url (%s), set to empty: %s", route, err)
+			return
+		}
+		trustyEndpoints.HTTPRouteURI = routeURL.String()
+		if httpScheme == routeURL.Scheme {
+			trustyEndpoints.WSRouteURI = fmt.Sprintf("%s://%s", webSocketScheme, routeURL.Host)
+		} else {
+			trustyEndpoints.WSRouteURI = fmt.Sprintf("%s://%s", webSocketSecureScheme, routeURL.Host)
+		}
+	}
+	return
+}
