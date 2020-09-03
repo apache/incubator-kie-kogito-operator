b6258582afb70984e7f641f3965044cdad20e336
Tarun Khandelwal <tarkhand@redhat.com>
2020-09-03 20:09:19 +0530

    [KOGITO-3104] - Missing Resources for Operator CRs (#544)
    See: https://issues.redhat.com/browse/KOGITO-3104
    
    Signed-off-by: Tarun Khandelwal <tarkhand@redhat.com>

--- pkg/apis/app/v1alpha1/kogitomgmtconsole_types.go
+++ pkg/apis/app/v1alpha1/kogitomgmtconsole_types.go
@@ -42,10 +42,10 @@ type KogitoMgmtConsoleStatus struct {
 // +kubebuilder:printcolumn:name="Image",type="string",JSONPath=".status.image",description="Base image for this service"
 // +kubebuilder:printcolumn:name="Endpoint",type="string",JSONPath=".status.externalURI",description="External URI to access this service"
 // +operator-sdk:gen-csv:customresourcedefinitions.displayName="Kogito Management Console"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Deployments,apps/v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Services,v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="ImageStreams,image.openshift.io/v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Routes,route.openshift.io/v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Deployment,apps/v1,\"A Kubernetes Deployment\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Service,v1,\"A Kubernetes Service\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="ImageStream,image.openshift.io/v1,\"A Openshift ImageStream\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Route,route.openshift.io/v1,\"A Openshift Route\""
 type KogitoMgmtConsole struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
--- pkg/apis/app/v1alpha1/kogitoruntime_types.go
+++ pkg/apis/app/v1alpha1/kogitoruntime_types.go
@@ -59,10 +59,10 @@ type KogitoRuntimeStatus struct {
 // +kubebuilder:printcolumn:name="Image",type="string",JSONPath=".status.image",description="Image of this service"
 // +kubebuilder:printcolumn:name="Endpoint",type="string",JSONPath=".status.externalURI",description="External URI to access this service"
 // +operator-sdk:gen-csv:customresourcedefinitions.displayName="Kogito service"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Deployments,apps/v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Routes,route.openshift.io/v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="ConfigMaps,v1"
-// +operator-sdk:gen-csv:customresourcedefinitions.resources="Services,v1"
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Deployment,apps/v1,\"A Kubernetes Deployment\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Route,route.openshift.io/v1,\"A Openshift Route\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="ConfigMap,v1,\"A Kubernetes ConfigMap\""
+// +operator-sdk:gen-csv:customresourcedefinitions.resources="Service,v1,\"A Kubernetes Service\""
 type KogitoRuntime struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
