7417b60319fae10500e378aaac7e70409622053c
jake <jacopo.r00ta@gmail.com>
2020-09-14 17:24:54 +0200

    cherry pick explainability pr

---
+++ cmd/kogito/command/install/explainability.go
@@ -0,0 +1,121 @@
+// Copyright 2020 Red Hat, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package install
+
+import (
+	"github.com/kiegroup/kogito-cloud-operator/cmd/kogito/command/context"
+	"github.com/kiegroup/kogito-cloud-operator/cmd/kogito/command/converter"
+	"github.com/kiegroup/kogito-cloud-operator/cmd/kogito/command/flag"
+	"github.com/kiegroup/kogito-cloud-operator/cmd/kogito/command/shared"
+	"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1"
+	"github.com/kiegroup/kogito-cloud-operator/pkg/infrastructure"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/spf13/cobra"
+)
+
+type installExplainabilityFlags struct {
+	flag.InstallFlags
+	flag.KafkaFlags
+}
+
+type installExplainabilityCommand struct {
+	context.CommandContext
+	command *cobra.Command
+	flags   installExplainabilityFlags
+	Parent  *cobra.Command
+}
+
+func initInstallExplainabilityCommand(ctx *context.CommandContext, parent *cobra.Command) context.KogitoCommand {
+	cmd := &installExplainabilityCommand{
+		CommandContext: *ctx,
+		Parent:         parent,
+	}
+
+	cmd.RegisterHook()
+	cmd.InitHook()
+
+	return cmd
+}
+
+func (i *installExplainabilityCommand) InitHook() {
+	i.flags = installExplainabilityFlags{}
+	i.Parent.AddCommand(i.command)
+	flag.AddInstallFlags(i.command, &i.flags.InstallFlags)
+	flag.AddKafkaFlags(i.command, &i.flags.KafkaFlags)
+}
+
+func (i *installExplainabilityCommand) Command() *cobra.Command {
+	return i.command
+}
+
+func (i *installExplainabilityCommand) RegisterHook() {
+	i.command = &cobra.Command{
+		Use:     "explainability [flags]",
+		Short:   "Installs the Kogito Explainability Service in the given Project",
+		Example: "explainability -p my-project",
+		Long: `'install explainability' will deploy the Explainability service to provide analysis on the decisions that have been taken by a kogito runtime application.
+
+If kafka-url is provided, it will be used to connect to the external Kafka server that is deployed in other project or infrastructure.
+If kafka-instance is provided instead, the value will be used as the Strimzi Kafka instance name to locate the Kafka server deployed in the Explainability service's project.
+Otherwise, the operator will try to deploy a Kafka instance via Strimzi operator for you using Kogito Infrastructure in the given project.`,
+		RunE:    i.Exec,
+		PreRun:  i.CommonPreRun,
+		PostRun: i.CommonPostRun,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if err := flag.CheckInstallArgs(&i.flags.InstallFlags); err != nil {
+				return err
+			}
+			if err := flag.CheckKafkaArgs(&i.flags.KafkaFlags); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+}
+
+func (i *installExplainabilityCommand) Exec(cmd *cobra.Command, args []string) error {
+	var err error
+	if i.flags.Project, err = shared.EnsureProject(i.Client, i.flags.Project); err != nil {
+		return err
+	}
+
+	kogitoExplainability := v1alpha1.KogitoExplainability{
+		ObjectMeta: metav1.ObjectMeta{Name: infrastructure.DefaultExplainabilityName, Namespace: i.flags.Project},
+		Spec: v1alpha1.KogitoExplainabilitySpec{
+			KogitoServiceSpec: v1alpha1.KogitoServiceSpec{
+				Replicas:              &i.flags.Replicas,
+				Envs:                  converter.FromStringArrayToEnvs(i.flags.Env, i.flags.SecretEnv),
+				Image:                 converter.FromImageFlagToImage(&i.flags.ImageFlags),
+				Resources:             converter.FromPodResourceFlagsToResourceRequirement(&i.flags.PodResourceFlags),
+				HTTPPort:              i.flags.HTTPPort,
+				InsecureImageRegistry: i.flags.ImageFlags.InsecureImageRegistry,
+			},
+			KafkaMeta: converter.FromKafkaFlagsToKafkaMeta(&i.flags.KafkaFlags, true),
+		},
+		Status: v1alpha1.KogitoExplainabilityStatus{
+			KogitoServiceStatus: v1alpha1.KogitoServiceStatus{
+				ConditionsMeta: v1alpha1.ConditionsMeta{Conditions: []v1alpha1.Condition{}},
+			},
+		},
+	}
+
+	return shared.
+		ServicesInstallationBuilder(i.Client, i.flags.Project).
+		SilentlyInstallOperatorIfNotExists(shared.KogitoChannelType(i.flags.Channel)).
+		WarnIfDependenciesNotReady(false, i.flags.KafkaFlags.UseKogitoInfra).
+		InstallExplainability(&kogitoExplainability).
+		GetError()
+}
--- pkg/apis/app/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/app/v1alpha1/zz_generated.openapi.go
@@ -35,6 +35,9 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoDataIndex":                schema_pkg_apis_app_v1alpha1_KogitoDataIndex(ref),
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoDataIndexSpec":            schema_pkg_apis_app_v1alpha1_KogitoDataIndexSpec(ref),
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoDataIndexStatus":          schema_pkg_apis_app_v1alpha1_KogitoDataIndexStatus(ref),
+		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainability":           schema_pkg_apis_app_v1alpha1_KogitoExplainability(ref),
+		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilitySpec":       schema_pkg_apis_app_v1alpha1_KogitoExplainabilitySpec(ref),
+		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilityStatus":     schema_pkg_apis_app_v1alpha1_KogitoExplainabilityStatus(ref),
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoInfra":                    schema_pkg_apis_app_v1alpha1_KogitoInfra(ref),
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoInfraSpec":                schema_pkg_apis_app_v1alpha1_KogitoInfraSpec(ref),
 		"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoInfraStatus":              schema_pkg_apis_app_v1alpha1_KogitoInfraStatus(ref),
@@ -578,6 +581,213 @@ func schema_pkg_apis_app_v1alpha1_KogitoDataIndexStatus(ref common.ReferenceCall
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
 				Description: "KogitoDataIndexStatus defines the observed state of KogitoDataIndex.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"conditions": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "History of conditions for the resource",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.Condition"),
+									},
+								},
+							},
+						},
+					},
+					"deploymentConditions": {
+						SchemaProps: spec.SchemaProps{
+							Description: "General conditions for the Kogito Service deployment.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("k8s.io/api/apps/v1.DeploymentCondition"),
+									},
+								},
+							},
+						},
+					},
+					"image": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Image is the resolved image for this service.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"externalURI": {
+						SchemaProps: spec.SchemaProps{
+							Description: "URI is where the service is exposed.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+				Required: []string{"conditions"},
+			},
+		},
+		Dependencies: []string{
+			"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.Condition", "k8s.io/api/apps/v1.DeploymentCondition"},
+	}
+}
+
+func schema_pkg_apis_app_v1alpha1_KogitoExplainability(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "KogitoExplainability defines the Explainability Service infrastructure deployment.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilitySpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilityStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilitySpec", "github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KogitoExplainabilityStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+	}
+}
+
+func schema_pkg_apis_app_v1alpha1_KogitoExplainabilitySpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "KogitoExplainabilitySpec defines the desired state of KogitoExplainability.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"replicas": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Number of replicas that the service will have deployed in the cluster. Default value: 1.",
+							Type:        []string{"integer"},
+							Format:      "int32",
+						},
+					},
+					"envs": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Environment variables to be added to the runtime container. Keys must be a C_IDENTIFIER.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("k8s.io/api/core/v1.EnvVar"),
+									},
+								},
+							},
+						},
+					},
+					"image": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Image definition for the service. Example: Domain: quay.io, Namespace: kiegroup, Name: kogito-service, Tag: latest. On OpenShift an ImageStream will be created in the current namespace pointing to the given image.",
+							Ref:         ref("github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.Image"),
+						},
+					},
+					"insecureImageRegistry": {
+						SchemaProps: spec.SchemaProps{
+							Description: "A flag indicating that image streams created by Kogito Operator should be configured to allow pulling from insecure registries. Usable just on OpenShift. Defaults to 'false'.",
+							Type:        []string{"boolean"},
+							Format:      "",
+						},
+					},
+					"resources": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Defined compute resource requirements for the deployed service.",
+							Ref:         ref("k8s.io/api/core/v1.ResourceRequirements"),
+						},
+					},
+					"deploymentLabels": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Additional labels to be added to the Deployment and Pods managed by the operator.",
+							Type:        []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"serviceLabels": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Additional labels to be added to the Service managed by the operator.",
+							Type:        []string{"object"},
+							AdditionalProperties: &spec.SchemaOrBool{
+								Allows: true,
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Type:   []string{"string"},
+										Format: "",
+									},
+								},
+							},
+						},
+					},
+					"httpPort": {
+						SchemaProps: spec.SchemaProps{
+							Description: "HttpPort will set the environment env HTTP_PORT to define which port service will listen internally.",
+							Type:        []string{"integer"},
+							Format:      "int32",
+						},
+					},
+					"kafka": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Has the data used by the service to connect to the Kafka cluster.",
+							Ref:         ref("github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KafkaConnectionProperties"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.Image", "github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1.KafkaConnectionProperties", "k8s.io/api/core/v1.EnvVar", "k8s.io/api/core/v1.ResourceRequirements"},
+	}
+}
+
+func schema_pkg_apis_app_v1alpha1_KogitoExplainabilityStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "KogitoExplainabilityStatus defines the observed state of KogitoExplainability.",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
 					"conditions": {
