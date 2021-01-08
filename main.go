47cd40ee30394f345233ea4e1b862ba26d1b4808
Tarun Khandelwal <tarkhand@redhat.com>
2021-01-08 14:23:30 +0530

    [KOGITO-4110] - Single namespaced operator reacts on CRs created in different namespaces See: https://issues.redhat.com/browse/KOGITO-4110
    Signed-off-by: Tarun Khandelwal <tarkhand@redhat.com>

--- main.go
+++ main.go
@@ -140,12 +140,13 @@ func getWatchNamespace() string {
 	// An empty value means the operator is running with cluster scope.
 	var watchNamespaceEnvVar = "WATCH_NAMESPACE"
 
-	ns, found := os.LookupEnv(watchNamespaceEnvVar)
-	if !found {
-		setupLog.Info("unable to get WatchNamespace, "+
-			"the manager will watch and manage resources in all namespaces",
+	ns, _ := os.LookupEnv(watchNamespaceEnvVar)
+
+	// Check if operator is running as cluster scoped
+	if len(ns) == 0 {
+		setupLog.Info(
+			"The operator is running as cluster scoped. It will watch and manage resources in all namespaces",
 			"Env Var lookup", watchNamespaceEnvVar)
-		return ""
 	}
 	return ns
 }
