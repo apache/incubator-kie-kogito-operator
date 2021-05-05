0782482b8c1788665e30ed3dfa2524aba2444977
Jenkins CI <jenkins@rhba-jenkins.rhev-ci-vms.eng.rdu2.redhat.com>
2021-05-05 04:35:47 -0400

    [1.6.x] Update snapshot version to 1.6.1-snapshot

--- cmd/kogito/version/version.go
+++ cmd/kogito/version/version.go
@@ -16,5 +16,5 @@ package version
 
 var (
 	// Version of Kogito CLI
-	Version = "1.6.0"
+	Version = "1.6.1-snapshot"
 )
--- version/version.go
+++ version/version.go
@@ -16,5 +16,5 @@ package version
 
 var (
 	// Version of Kogito Operator
-	Version = "1.6.0"
+	Version = "1.6.1-snapshot"
 )
