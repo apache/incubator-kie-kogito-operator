998e40d44c3ac67bd4e38280c4cb7bb4d39f5e7b
Ant Stephenson <anstephe@redhat.com>
2022-06-16 14:21:50 +0000

    [1.13.x] KIECLOUD-639 - Remove lookaside cache usage in OSBS

--- modules/org.kie.kogito.rhpam.builder/install.sh
+++ modules/org.kie.kogito.rhpam.builder/install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
 
-if [ -n "$CACHITO_ENV_FILE" ]; then
-  source $CACHITO_ENV_FILE && go build -a -o rhpam-kogito-operator-manager main.go
-  cp $REMOTE_SOURCE_DIR/app/rhpam-kogito-operator-manager /workspace
-else
-  cd /workspace
-  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o rhpam-kogito-operator-manager main.go;
-fi
+cd /workspace
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o rhpam-kogito-operator-manager main.go;
