f69c36b5bc35c40eba61b926d991a4d7718c5acd
spolti <fspolti@redhat.com>
2022-07-19 14:52:08 -0300

    [1.13.x-blue][KOGITO-7457] - Rebranding changes on the kogito-operator repository
    revert back the regitry.redhat.io
    
    Signed-off-by: spolti <fspolti@redhat.com>

--- examples/rhpam/quarkus-jvm.Dockerfile
+++ examples/rhpam/quarkus-jvm.Dockerfile
@@ -1,4 +1,4 @@
-FROM registry.connect.redhat.com/ibm-bamoe/bamoe-kogito-runtime-jvm-rhel8:8.0.0
+FROM registry.redhat.io/ibm-bamoe/bamoe-kogito-runtime-jvm-rhel8:8.0.0
 
 ENV RUNTIME_TYPE quarkus
 # How to use this image with a simple example:
--- hack/rhpam/rhpam-deploy-operator-on-ocp.sh
+++ hack/rhpam/rhpam-deploy-operator-on-ocp.sh
@@ -27,7 +27,7 @@ mkdir -p $TMP_FOLDER
 rm -rf $TMP_FOLDER/*
 
 # Apply all
-sed "s|image: registry.connect.redhat.com/ibm-bamoe/.*|image: $IMAGE|g" rhpam-operator.yaml > $TMP_FOLDER/rhpam-operator.yaml
+sed "s|image: registry.stage.redhat.io/ibm-bamoe/.*|image: $IMAGE|g" rhpam-operator.yaml > $TMP_FOLDER/rhpam-operator.yaml
 kubectl apply -f $TMP_FOLDER/rhpam-operator.yaml
 
 # Clean Up
