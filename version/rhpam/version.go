6e5c37019a2b4be92bfd6aa8002dbd2d164c641f
Filippe Spolti <fspolti@redhat.com>
2022-09-06 02:14:58 -0300

    [KIECLOUD-654] - prepare RHPAM and RHPAM Kogito operators for 7.13.1 release (#1273)
    Signed-off-by: spolti <fspolti@redhat.com>
    
    Signed-off-by: spolti <fspolti@redhat.com>

--- version/rhpam/version.go
+++ version/rhpam/version.go
@@ -16,11 +16,11 @@ package rhpam
 
 var (
 	// PriorVersion product version
-	PriorVersion = "7.12.0"
+	PriorVersion = "7.13.0"
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-4"
+	CsvPriorVersion = PriorVersion + "-1"
 	// Version - current version
-	Version = "7.13.0"
+	Version = "7.13.1"
 	// CsvVersion - csv release
 	CsvVersion = Version + "-1"
 )
