b68b8ebca377b90c7bc6dc8a1c58585abdfeb94e
Filippe Spolti <fspolti@redhat.com>
2022-09-20 08:35:45 -0300

    [KOGITO-7922] - Update Kogito Operator fo Go 1.17 (#1280)
    * [KOGITO-7920] - Update Kogito Operator fo Go 1.17
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * podman
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * test
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * Set test nodes
    
    * update pipelines
    
    * Use go install instead of go get
    
    * forgot some
    
    * correction
    
    * go get
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * updated Makefile
    
    * updated test mod file
    
    * updated test go mod
    
    * updates
    
    * updated operator-sdk version
    
    Set to latest built on Golang 1.17
    
    * updated script
    
    * cleanup
    
    * [KOGITO-7922] - Update Kogito Operator to Go 1.17 and operator-sdk to 1.21
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * initial tests cucumber upgrade
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * few more additions
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * updated deps
    
    * correct compilation problems
    
    Signed-off-by: spolti <fspolti@redhat.com>
    Co-authored-by: radtriste <tradisso@redhat.com>

--- controllers/rhpam/suite_test.go
+++ controllers/rhpam/suite_test.go
@@ -15,6 +15,7 @@
 package rhpam
 
 import (
+	"k8s.io/client-go/rest"
 	"path/filepath"
 	"testing"
 
@@ -34,8 +35,11 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	cfg       *rest.Config
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -54,7 +58,9 @@ var _ = BeforeSuite(func() {
 		ErrorIfCRDPathMissing: true,
 	}
 
-	cfg, err := testEnv.Start()
+	var err error
+	// cfg is defined in this file globally.
+	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
