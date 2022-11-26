3b86357b5379f9d886f4b3728ed286d03030637f
spolti <fspolti@redhat.com>
2022-11-26 12:26:20 -0300

    updade bundle to 7.13.1-2
    Signed-off-by: spolti <fspolti@redhat.com>

--- version/rhpam/version.go
+++ version/rhpam/version.go
@@ -16,11 +16,11 @@ package rhpam
 
 var (
 	// PriorVersion product version
-	PriorVersion = "7.13.0"
+	PriorVersion = "7.13.1"
 	// CsvPriorVersion - prior csv release
 	CsvPriorVersion = PriorVersion + "-1"
 	// Version - current version
 	Version = "7.13.1"
 	// CsvVersion - csv release
-	CsvVersion = Version + "-1"
+	CsvVersion = Version + "-2"
 )
