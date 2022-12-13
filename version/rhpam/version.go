c4b96c376c0742d31075e4cefe47aa49ce946150
Filippe Spolti <fspolti@redhat.com>
2022-12-13 19:27:13 -0300

    [1.13.x][KIECLOUD-656] - Prepare RHPAM branches for 7.13.2  (#1330)
    * updade bundle to 7.13.1-2 (#1320)
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    * [KIECLOUD-656] - Prepare RHPAM branches for 7.13.2
    
    Signed-off-by: spolti <fspolti@redhat.com>
    
    Signed-off-by: spolti <fspolti@redhat.com>

--- version/rhpam/version.go
+++ version/rhpam/version.go
@@ -16,11 +16,11 @@ package rhpam
 
 var (
 	// PriorVersion product version
-	PriorVersion = "7.13.0"
+	PriorVersion = "7.13.1"
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-1"
+	CsvPriorVersion = PriorVersion + "-2"
 	// Version - current version
-	Version = "7.13.1"
+	Version = "7.13.2"
 	// CsvVersion - csv release
 	CsvVersion = Version + "-1"
 )
