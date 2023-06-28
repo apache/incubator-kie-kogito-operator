3c2d9e5a29a3bbcec8dc3d29db1e4b856ed67e55
Your Name <fspolti@redhat.com>
2023-06-28 11:45:43 -0300

    [KIECLOUD-725] - Update RHPAM and BAMOE bundle for CVE respin
    Signed-off-by: Your Name <fspolti@redhat.com>

--- version/rhpam/version.go
+++ version/rhpam/version.go
@@ -15,12 +15,12 @@
 package rhpam
 
 var (
-	// PriorVersion product version
-	PriorVersion = "8.0.2"
-	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-3"
 	// Version - current version
 	Version = "8.0.3"
+	// PriorVersion product version
+	PriorVersion = "8.0.2"
 	// CsvVersion - csv release
-	CsvVersion = Version + "-1"
+	CsvVersion = Version + "-2"
+	// CsvPriorVersion - prior csv release
+	CsvPriorVersion = Version + "-1"
 )
